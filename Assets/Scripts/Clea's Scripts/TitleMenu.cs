7d36ba0dee407da70e91e3a87b0d6beb57e9bd0d
Channa188 <clea_h@p-hall.net>
2022-01-22 23:54:36 -0800

    Title Menu Update
    Play button now works

--- Assets/Scripts/Clea's Scripts/TitleMenu.cs
+++ Assets/Scripts/Clea's Scripts/TitleMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleMenu : MonoBehaviour
 {
@@ -32,5 +33,9 @@ public class TitleMenu : MonoBehaviour
 
     }
 
+    public void Play()
+    {
+        SceneManager.LoadScene("StartingArea");
+    }
 
 }
