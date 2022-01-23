4c651d1fc57d2009817936b8c5f576ae50880305
Richard Cheng <richard.hokan.cheng@gmail.com>
2022-01-23 00:13:51 -0800

    Player jumping and animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,7 +5,9 @@ using UnityEngine.InputSystem;
 
 [DisallowMultipleComponent]
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))] // Should be a trigger collider
 [RequireComponent(typeof(PlayerInput))]
+[RequireComponent(typeof(Animator))]
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -14,24 +16,69 @@ public class PlayerMovement : MonoBehaviour
 		[Tooltip("Speed at which player moves horizontally.")]
 		[SerializeField] private float speed = 5.0f;
 
+		[Tooltip("How high the player jumps.")]
+		[SerializeField] private float jumpHeight = 5.0f;
+
 	// The current player input for movement
 	private Vector2 movement = Vector2.zero;
 
+	// How many ground objects the player is touching
+	private int grounded = 0;
+
 	private Rigidbody2D rb;
+	private Animator animator;
 
 	void Awake()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
 	}
 
 	// Update movement when player inputs controls
 	void OnMove(InputValue movementValues)
 	{
 		movement = movementValues.Get<Vector2>();
+
+		if (movement.x == 0)
+		{
+			animator.SetBool("isRunning", false);
+		}
+		else
+		{
+			animator.SetBool("isRunning", true);
+			transform.localScale = new Vector3(transform.localScale.y * (movement.x > 0 ? 1 : -1), transform.localScale.y, transform.localScale.z);
+		}
 	}
 
 	void FixedUpdate()
 	{
-		rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
+		if (grounded > 0 && movement.y > 0)
+		{
+			rb.velocity = new Vector2(movement.x * speed, movement.y * jumpHeight);
+			animator.ResetTrigger("landed");
+			animator.SetBool("isJumping", true);
+		}
+		else
+		{
+			rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Ground")
+		{
+			grounded++;
+			animator.SetBool("isJumping", false);
+			animator.SetTrigger("landed");
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Ground")
+		{
+			grounded--;
+		}
 	}
 }
